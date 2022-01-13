b95c606f62940880d9bb67a552c6b07114bdf4c8
Fabio Luis Girardi <fabio@pascalscada.com>
2022-01-13 17:14:51 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,19 +6,31 @@ const githubToken = core.getInput('github-token')
 
 const client = github.getOctokit(githubToken)
 
-labels = await octokit.request('GET /repos/{owner}/{repo}/labels', {
+const push = github.context.payload.push
+
+if (!push) {
+  throw new Error(
+    'Could not get push from context'
+  )
+}
+
+var labels = await octokit.request('GET /repos/{owner}/{repo}/labels', {
   owner: github.context.repo.owner,
   repo: github.context.repo.repo
 })
 
 for (label in labels)
 {
-  
-  int IssueNumber;
-  await client.issues.addLabels({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    issue_number: IssueNumber,
-    labels: label
-  })
+  var regexp = new RegExp('(%'+label.name+'% #)(\d+)(.*)'),
+  for (commit in push.commits)
+  {
+    
+    var IssueNumber;
+    await client.issues.addLabels({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      issue_number: IssueNumber,
+      labels: label
+    })
+  }
 }
