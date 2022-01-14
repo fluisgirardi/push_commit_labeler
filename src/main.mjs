3add16a4f74cefdeb1d1fcf5de85d7168027aa9e
Fabio Luis Girardi <fabio@pascalscada.com>
2022-01-14 09:47:43 -0300

    Update main.mjs

--- src/main.mjs
+++ src/main.mjs
@@ -8,39 +8,42 @@ const client = github.getOctokit(githubToken)
 
 const push = github.context.payload.push
 
-if (!push) {
-  throw new Error(
-    'Could not get push from context'
-  )
-}
-
-var labels = await octokit.request('GET /repos/{owner}/{repo}/labels', {
-  owner: github.context.repo.owner,
-  repo: github.context.repo.repo
-})
-
-for (label in labels)
+function main_run()
 {
-  var regexp = new RegExp('(%'+label.name+'% #)(\d+)(.*)')
+  if (!push) {
+    throw new Error(
+      'Could not get push from context'
+    )
+  }
   
-  for (commit in push.commits)
+  var labels = await octokit.request('GET /repos/{owner}/{repo}/labels', {
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo
+  })
+  
+  for (label in labels)
   {
-    var regex = new RegExp('((%'+label+'% #)(\\d+))','gmi');
-    var results = [...commit.message.matchAll(regex)];
-    if (result.length>0)
+    var regexp = new RegExp('(%'+label.name+'% #)(\d+)(.*)')
+    
+    for (commit in push.commits)
     {
-       for (r in results)
-       {
-         var IssueNumber = parseInt(r[3]);
-         if isNaN(IssueNumber) continue;
-         
-         await client.issues.addLabels({
-           owner: github.context.repo.owner,
-           repo: github.context.repo.repo,
-           issue_number: IssueNumber,
-           labels: label
-         })
-       }
+      var regex = new RegExp('((%'+label+'% #)(\\d+))','gmi');
+      var results = [...commit.message.matchAll(regex)];
+      if (result.length>0)
+      {
+         for (r in results)
+         {
+           var IssueNumber = parseInt(r[3]);
+           if isNaN(IssueNumber) continue;
+           
+           await client.issues.addLabels({
+             owner: github.context.repo.owner,
+             repo: github.context.repo.repo,
+             issue_number: IssueNumber,
+             labels: label
+           })
+         }
+      }
     }
   }
 }
