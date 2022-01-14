dcebed2fcbc374757cc56327d07087b9c0038c41
Fabio Luis Girardi <fabio@pascalscada.com>
2022-01-14 11:33:55 -0300

    %bug% #1 %test% #2

--- index.js
+++ index.js
@@ -6,58 +6,58 @@ const githubToken = core.getInput('github-token')
 
 const client = github.getOctokit(githubToken)
 
+core.error('This is a bad error. This will also fail the build.')
 
-function main_run()
+core.warning('Something went wrong, but it\'s not bad enough to fail the build.')
+
+core.notice('Something happened that you might want to know about.')
+
+if (!github.context.payload) 
 {
-  if (!github.context.payload) 
-  {
-    throw new Error('No payload found in the context.')
-  }
+  throw new Error('No payload found in the context.')
+}
 
-  if ((!github.context.payload.commits) || (!github.context.payload.commits.length)) 
-  {
-    console.log(' - skipping commits');
-    return;
-  }
-  
-  var labels = client.request('GET /repos/{owner}/{repo}/labels', {
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo
-  });
+if ((!github.context.payload.commits) || (!github.context.payload.commits.length)) 
+{
+  console.log(' - skipping commits');
+  return;
+}
 
-  console.log(labels);
+var labels = client.request('GET /repos/{owner}/{repo}/labels', {
+  owner: github.context.repo.owner,
+  repo: github.context.repo.repo
+});
+
+console.log(labels);
+
+for (label in labels)
+{
+  var regexp = new RegExp('(%'+label.name+'% #)(\d+)(.*)');
+  console.log('Looking for commit messages with %'+label.name);
   
-  for (label in labels)
+  for (const i in github.context.payload.commits)
   {
-    var regexp = new RegExp('(%'+label.name+'% #)(\d+)(.*)');
-    console.log('Looking for commit messages with %'+label.name);
-    
-    for (const i in github.context.payload.commits)
+    if (!github.context.payload.commits[i].message) continue;
+    console.log('Commit message'+i+' - '+github.context.payload.commits[i].message);
+    var regex = new RegExp('((%'+label+'% #)(\\d+))','gmi');
+    var results = [...github.context.payload.commits[i].message.matchAll(regex)];
+    console.log(results);
+    if (result.length>0)
     {
-      if (!github.context.payload.commits[i].message) continue;
-      console.log('Commit message'+i+' - '+github.context.payload.commits[i].message);
-
-      var regex = new RegExp('((%'+label+'% #)(\\d+))','gmi');
-      var results = [...github.context.payload.commits[i].message.matchAll(regex)];
-      console.log(results);
-      if (result.length>0)
+      for (r in results)
       {
-         for (r in results)
-         {
-           var IssueNumber = parseInt(r[3]);
-           if (isNaN(IssueNumber)) continue;
-           
-           client.issues.addLabels({
-             owner: github.context.repo.owner,
-             repo: github.context.repo.repo,
-             issue_number: IssueNumber,
-             labels: label
-           })
-         }
+        var IssueNumber = parseInt(r[3]);
+        if (isNaN(IssueNumber)) continue;
+         
+        client.issues.addLabels({
+          owner: github.context.repo.owner,
+          repo: github.context.repo.repo,
+          issue_number: IssueNumber,
+          labels: label
+        })
       }
     }
   }
 }
 
-main_run();
 
